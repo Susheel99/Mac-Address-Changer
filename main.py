87c951564d58d0e09e4a9e2a65086a1502a2355b
Susheel99 <asusheel23b@gmail.com>
2021-10-19 17:39:33 +0100

    User input check

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's Mac address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
 
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -18,5 +24,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
