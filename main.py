19de17b1198910bdba026993783f20da0a13e82c
Susheel99 <asusheel23b@gmail.com>
2021-10-19 17:28:48 +0100

    added functions

--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's Mac address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's Mac address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
 
-(options, arguments) = parser.parse_args()
+    return parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
 
-print("[+] Changing MAC address for "+interface+" to "+new_mac)
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
